142c2d0cfe8faf762c89f1a72fb3daf65b6060fa
Mithun Sivapathasundram <75385676+mithun-s14@users.noreply.github.com>
2024-05-28 16:12:50 -0400

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,7 +17,9 @@ const Projects = () => {
                     Smart Recruiter - <span className='text-sm text-[#38A5FF]'>OPS Phenomenal Hackathon</span>
                   </p>
                   <ol className='text-gray-300 mb-4 text-sm'>
-                    <li className='py-1'>Built apps</li>
+                    <li className='py-1'>Developed an all-in-one recruitment platform securing 2nd place in the OPS Phenomenal Hackathon by leveraging Microsoft's tools to significantly
+                    reduce time spent during the hiring process</li>
+                    <li className='py-1'>Utilized Microsoft's 'Category Classification Model' to detect the likelihood of an application being composed by AI</li>
                   </ol>
                   <div className='flex flex-wrap space-x-2'>
                     <span className='mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]'>Microsoft Power Apps</span>
@@ -28,22 +30,22 @@ const Projects = () => {
                 </div>
               </div>
             </div>
-
+            
             <div className='w-full mt-8'>
               <div className='flex flex-col sm:flex-row justify-between items-start sm:items-center'>
                 
                 <div className='flex flex-col'>
                   <p className='mb-2 font-semibold text-lg'>
-                    Soccer League Database System - <span className='text-sm text-[#38A5FF]'>OPS Phenomenal Hackathon</span>
+                    Portfolio - <span className='text-sm text-[#38A5FF]'>Personal Project</span>
                   </p>
                   <ol className='text-gray-300 mb-4 text-sm'>
-                    <li className='py-1'>Built apps</li>
+                    <li className='py-1'>Developed a website to showcase more about me and hone my skills in React and Tailwind CSS</li>
                   </ol>
                   <div className='flex flex-wrap space-x-2'>
-                    <span className='mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]'>Microsoft Power Apps</span>
+                    <span className='mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]'>HTML</span>
                     <span className='mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]'>JavaScript</span>
-                    <span className='mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]'>REST APIs</span>
-                    <span className='mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]'>Machine Learning Models</span>
+                    <span className='mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]'>Tailwind CSS</span>
+                    <span className='mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]'>React</span>
                   </div>
                 </div>
               </div>
@@ -54,16 +56,16 @@ const Projects = () => {
                 
                 <div className='flex flex-col'>
                   <p className='mb-2 font-semibold text-lg'>
-                    Java Stock Market System - <span className='text-sm text-[#38A5FF]'>OPS Phenomenal Hackathon</span>
+                    Soccer League Database System - <span className='text-sm text-[#38A5FF]'>Toronto Metropolitan University</span>
                   </p>
                   <ol className='text-gray-300 mb-4 text-sm'>
-                    <li className='py-1'>Built apps</li>
+                    <li className='py-1'>Built a relational database management system using SQL and Oracle DB to track all information needed for a soccer league</li>
+                    <li className='py-1'>Conducted comprehensive testing protocols to verify data integrity, consistency, and reliability</li>
                   </ol>
                   <div className='flex flex-wrap space-x-2'>
-                    <span className='mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]'>Microsoft Power Apps</span>
-                    <span className='mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]'>JavaScript</span>
-                    <span className='mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]'>REST APIs</span>
-                    <span className='mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]'>Machine Learning Models</span>
+                    <span className='mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]'>SQL</span>
+                    <span className='mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]'>Oracle DB</span>
+                    <span className='mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]'>GitHub</span>
                   </div>
                 </div>
               </div>
@@ -74,16 +76,42 @@ const Projects = () => {
                 
                 <div className='flex flex-col'>
                   <p className='mb-2 font-semibold text-lg'>
-                    NBA Stats - <span className='text-sm text-[#38A5FF]'>OPS Phenomenal Hackathon</span>
+                    Java Stock Market System - <span className='text-sm text-[#38A5FF]'>Toronto Metropolitan University</span>
                   </p>
                   <ol className='text-gray-300 mb-4 text-sm'>
-                    <li className='py-1'>Built apps</li>
+                    <li className='py-1'>Collaborated with 5 developers to develop, design, troubleshoot, and document an object-oriented system that
+                    simulates the purchasing and selling of stocks using real-time data</li>
+                    <li className='py-1'>Engineered comprehensive UML models to optimize system architecture for objects</li>
+                    <li className='py-1'>Automated unit testing by leverging Java's JUnit testing framework</li>
                   </ol>
                   <div className='flex flex-wrap space-x-2'>
-                    <span className='mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]'>Microsoft Power Apps</span>
-                    <span className='mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]'>JavaScript</span>
-                    <span className='mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]'>REST APIs</span>
-                    <span className='mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]'>Machine Learning Models</span>
+                    <span className='mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]'>Java</span>
+                    <span className='mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]'>J Unit</span>
+                    <span className='mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]'>Alpha Vantage API</span>
+                    <span className='mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]'>GitHub</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className='w-full mt-8'>
+              <div className='flex flex-col sm:flex-row justify-between items-start sm:items-center'>
+                
+                <div className='flex flex-col'>
+                  <p className='mb-2 font-semibold text-lg'>
+                    NBA Stats - <span className='text-sm text-[#38A5FF]'>Personal Project</span>
+                  </p>
+                  <ol className='text-gray-300 mb-4 text-sm'>
+                    <li className='py-1'>Created a Python application to retrieve, clean, and evaluate data from NBA players to learn more about data analysis in Python</li>
+                    <li className='py-1'>Constructed interactive shot charts to visually depict players' shooting performance throughout a given season</li>
+                  </ol>
+                  <div className='flex flex-wrap space-x-2'>
+                    <span className='mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]'>Python</span>
+                    <span className='mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]'>NBA_API</span>
+                    <span className='mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]'>Beautiful Soup</span>
+                    <span className='mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]'>Requests</span>
+                    <span className='mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]'>Pandas</span>
+                    <span className='mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]'>Matplotlib</span>
                   </div>
                 </div>
               </div>
